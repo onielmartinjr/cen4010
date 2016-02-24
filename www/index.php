fc28aecdfaa0570ddb7423d683272b08713877ef
Nick Jones <tsip_67@yahoo.com>
2016-02-24 10:12:56 -0500

    reorganized the folders

--- www/index.php
+++ www/index.php
@@ -2,26 +2,43 @@
 
 	//require the framework
 	require_once "requires/initialize.php";
-
+	
+	$page_var = new Page();
+	$page_var->name = "Homepage";
+	$page_var->body = "This is the body";
+	
 ?>
-
-<!--
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Title</title>
-	</head>
+<head>
 
+	<title><?php echo $page_var->name;?></title>
+
+</head>
 <body>
-Body content goes here.
-</body>
 
-</html>
--->
-
-<?php	
+	<h1><?php echo $page_var->name;?></h1>
 	
+	<nav>
+		<a href="index.php">Homepage</a><br />
+		<a href="login.php">Login Here!</a><br />
+		<a href="public1.php">Public 1</a><br />
+		<a href="member1.php">Member 1</a><br />
+		<a href="admin1.php">Admin 1</a><br />
+	</nav> <br />
+	
+	<?php 
+		display_error();
+		
+		echo $page_var->body;
+	?>
+	
+
+</body>
+</html>
+
+<?php
+
 	//close connection
 	$database->close_connection();
 
