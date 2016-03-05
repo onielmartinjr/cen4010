014d09f92590edd5447773dc5955ab7338c2a05b
Nick Jones <tsip_67@yahoo.com>
2016-03-05 15:10:26 -0500

    Issue resolved: delete account. Added account managment page, delete account page, update session logout function to bypass message and redirect, update navigation page

---
+++ www/delete_account.php
@@ -0,0 +1,53 @@
+<?php
+
+	//require the framework
+	require_once "requires/initialize.php";
+	
+	// create the page
+	$page = new Page();
+	$page->name = "Delete Account";
+	$page->is_user_only = true;
+	
+	// check if user wants to delete their account
+	if (isset($_POST["confirm"]))
+	{
+		if (in_array($user->role_wk, array("2", "3"))) // if logged in as ADMIN or STAFF
+		{
+			// if user is the last admin or staff, cannot delete account
+			$user_array = User::find_by_sql("SELECT * FROM `user` WHERE `role_wk` = " . $user->role_wk . " AND `is_deleted` = 0;"); // find all of the ADMINs
+			if (count($user_array) <= 1) // if last ADMIN of last STAFF account...
+			{
+				$session->message("You are the last " . $user->role_wk->name . "! Another " . $user->role_wk->name . " account must be created before this one can be deleted.");
+				redirect_head(ROOT_URL . "manage_account.php");
+				die();
+			}
+		}
+		
+		// delete the user
+		$user->delete();
+		$session->message("Your account has been deleted!");
+		$session->logout(true);
+		redirect_head(ROOT_URL . "index.php");
+		die();
+	}
+	elseif (isset($_POST["deny"]))
+	{
+		$session->message("Your account was not deleted.");
+		redirect_head(ROOT_URL . "manage_account.php");
+	}
+	
+	require_once "requires/template/header.php";
+	
+?>	
+	
+	<form id="confirm_delete" action="delete_account.php" method="post">
+		<label>Are you sure you want to delete your account?</label> <br />
+		<input type="submit" value="No, keep my account!" name="deny" />
+		<input type="submit" value="Yes, delete my account" name="confirm" />
+	</form>
+	
+<?php
+	
+	require_once "requires/template/footer.php";
+	
+?>
--- www/manage_account.php
+++ www/manage_account.php
@@ -3,15 +3,22 @@
 	//require the framework
 	require_once "requires/initialize.php";
 	
+	// create the page
 	$page = new Page();
-	$page->name = "Member1";
-	$page->body = "My body is for your user-eyes only ;)";
+	$page->name = "Account Management";
 	$page->is_user_only = true;
-		
+	
+	
 	require_once "requires/template/header.php";
-		
-	echo $page->body;
-
+	
+?>	
+	
+<br /><br />
+<a href="delete_account.php">Delete Account</a>
+<!-- Other account management links here -->
+	
+<?php
+	
 	require_once "requires/template/footer.php";
-
+	
 ?>
