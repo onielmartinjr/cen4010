2daafbdae2769b311fb73ebfa5c35813334e5459
onielmartinjr <martin.oniel@icloud.com>
2016-03-31 21:55:29 -0400

    Working on Watch List

--- www/user/manage_watch_lists.php
+++ www/user/manage_watch_lists.php
@@ -7,6 +7,34 @@
 	$page->name = "Manage Watch Lists";
 	$page->is_user_only = true;
 	
+	//if we're deleting a watch list
+	if(isset($_GET['delete_watch_list_wk'])) {
+		//grab the watch list
+		$to_delete = Watch_list::find_by_id($_GET['delete_watch_list_wk']);
+		
+		//if the watch list doesn't exist, error
+		if(!$to_delete) { 
+			//if the watch list does not exist
+			$session->message("There was an error in the URL; please try again.");
+		} else if($to_delete->user_wk->user_wk != $user->user_wk) {
+			//if the watch list does not belong to the current user
+			$session->message("You cannot delete someone else's watch list.");
+		} else {
+			//if we're here, we actually need to delete it
+			
+			//and all the entries from it
+			$sql = "DELETE FROM `watch_list_detail` WHERE `watch_list_wk` = ".$to_delete->watch_list_wk.";";
+			if($to_delete->delete() && $database->query($sql)) {
+				$session->message("Your watch list was successfully removed!");
+			} else {
+				$session->message("There was an error removing the watch list entry.");
+			}
+		}
+		
+		//redirect back
+		redirect_head(file_name_without_get());
+	}
+	
 	//grab all the watch lists for this user
 	$watch_lists = Watch_List::find_by_sql("SELECT * FROM `watch_list` WHERE `user_wk` = ".$user->user_wk.";");
 
@@ -15,7 +43,8 @@
 	if($watch_lists) {
 		$page->body .= "<p><em>Your current watch lists.</em><br />";
 		foreach($watch_lists AS $list) {
-			$page->body .= "<a href=\"update_watch_list.php?watch_list_wk=".$list->watch_list_wk."\">".$list->name."</a><br />";
+			$page->body .= $list->name."<br />";
+			$page->body .= "<a href=\"".file_name_without_get()."?delete_watch_list_wk=".$list->watch_list_wk."\" style=\"margin-left:1.5em;\" >Delete</a><br />";
 		}
 		$page->body .= "</p>";
 	} else {
