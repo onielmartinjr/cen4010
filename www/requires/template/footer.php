38504dbf1add8683403892a14c59f20294a5ba60
onielmartinjr <martin.oniel@icloud.com>
2016-04-19 20:52:23 -0400

    Issue: Utilize Global Website Settings

--- www/requires/template/footer.php
+++ www/requires/template/footer.php
@@ -9,8 +9,21 @@
                 <div class="col-sm-6">
                     <ul class="pull-right">
 <?php
+						
+						//display the social media links - IF THEY'RE SETUP IN THE SETTINGS
+						if(isset($website_settings['facebook_link']))
+							echo "<li><a href=\"".$website_settings['facebook_link']."\"><img src=\"".ROOT_URL."UI-links/images/social_media/facebook.png\" /></a></li>";
+
+						if(isset($website_settings['instagram_link']))
+							echo "<li><a href=\"".$website_settings['instagram_link']."\"><img src=\"".ROOT_URL."UI-links/images/social_media/instagram.png\" /></a></li>";
+							
+						if(isset($website_settings['twitter_link']))
+							echo "<li><a href=\"".$website_settings['twitter_link']."\"><img src=\"".ROOT_URL."UI-links/images/social_media/twitter.png\" /></a></li>";
+							
+						if(isset($website_settings['youtube_link']))
+							echo "<li><a href=\"".$website_settings['youtube_link']."\"><img src=\"".ROOT_URL."UI-links/images/social_media/youtube.png\" /></a></li>";
+							
 						//display static pages
-						//echo 'Public Pages<br />';
 						echo "<li><a style=\"padding-left:1.45em;\" href=\"".ROOT_URL."index.php\">Home</a></li>";
 						echo "<li><a style=\"padding-left:1.45em;\" href=\"".ROOT_URL."search_pets.php\">Search Pets</a></li>";
 						echo "<li><a style=\"padding-left:1.45em;\" href=\"".ROOT_URL."view_page.php?page_wk=2\">About Us</a></li>";
